1781d7f8e6185d8bb100017bdb29c45e64a141e2
JinQing Lee <hourout@163.com>
2021-09-09 16:04:37 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@
 # import paddleocr
 # from .paddleocr import *
 
-__version__ = paddleocr.VERSION
+# __version__ = paddleocr.VERSION
 __all__ = [
 #           'PaddleOCR', 'PPStructure', 'draw_ocr', 'draw_structure_result', 'save_structure_res','download_with_progressbar',
           'PaddleocrSAST']
